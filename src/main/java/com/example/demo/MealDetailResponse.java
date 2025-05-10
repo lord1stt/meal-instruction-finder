4039a1b850090a67df49e62c12382079aee40370
Ahmet Eyibil <103658826+lord1stt@users.noreply.github.com>
2025-05-10 12:43:18 +0300

    Update MealDetailResponse.java

--- src/main/java/com/example/demo/MealDetailResponse.java
+++ src/main/java/com/example/demo/MealDetailResponse.java
@@ -1,7 +1,8 @@
 package com.example.demo;
+import java.time.LocalDate;
 import java.util.List;
-public class MealDetailResponse {
 
+public class MealDetailResponse {
     private List<MealDetail> meals;
 
     public List<MealDetail> getMeals() {
@@ -13,33 +14,64 @@ public class MealDetailResponse {
     }
 
     public static class MealDetail {
-        private String strMeal;
-        private String strInstructions;
-        private String strMealThumb;
+        private int mealId;
+        private String mealNameStr;
+        private String mealInstructions;
+        private String mealImageUrl;
+        private boolean isFavorite;
+        private String addFavDate;
 
-        public String getStrMeal() {
-            return strMeal;
+        public int getMealId() {
+            return mealId;
         }
 
-        public void setStrMeal(String strMeal) {
-            this.strMeal = strMeal;
+        public void setMealId(int mealId) {
+            this.mealId = mealId;
+        }
+
+        public String getStrMeal() {
+            return mealNameStr;
+        }
+
+        public void setStrMeal(String mealNameStr) {
+            this.mealNameStr = mealNameStr;
         }
 
         public String getStrInstructions() {
-            return strInstructions;
+            return mealInstructions;
         }
 
-        public void setStrInstructions(String strInstructions) {
-            this.strInstructions = strInstructions;
+        public void setStrInstructions(String mealInstructions) {
+            this.mealInstructions = mealInstructions;
         }
 
-        public String getStrMealThumb() {
-            return strMealThumb;
+        public String getMealImageUrl() {
+            return mealImageUrl;
         }
 
-        public void setStrMealThumb(String strMealThumb) {
-            this.strMealThumb = strMealThumb;
+        public void setMealImageUrl(String mealImageUrl) {
+            this.mealImageUrl = mealImageUrl;
+        }
+
+        public boolean isFavorite() {
+            return isFavorite;
+        }
+
+        public void setFavorite(boolean favorite) {
+            isFavorite = favorite;
+            if (favorite) {
+                this.addFavDate = LocalDate.now().toString();
+            } else {
+                this.addFavDate = null;
+            }
+        }
+
+        public String getAddFavDate() {
+            return addFavDate;
+        }
+
+        public void setAddFavDate(String addFavDate) {
+            this.addFavDate = addFavDate;
         }
     }
 }
-
